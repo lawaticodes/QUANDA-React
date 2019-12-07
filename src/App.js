7b5aa8f4acef19917c52b988529fd9a193903894
tinalimbu <tinalawati@gmail.com>
2019-12-07 01:07:36 +0000

    Edited main component and forum component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import Forum from './forum/Forum.js';
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <div className="Header">
-          <p>This is the header.</p>
-      </div>
-      <div className="App">
-        <p>Welcome to Quanda.</p>
-      </div>
-      <div className="Footer">
-          <p>This is the footer.</p>
-      </div>
-    </React.Fragment>
-  );
+class Quanda extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="Header">
+            <p>This is the header.</p>
+        </div>
+        <div className="Content">
+          <p>Welcome to Quanda.</p>
+          <Forum />
+        </div>
+        <div className="Footer">
+            <p>This is the footer.</p>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+export default Quanda;
---
+++ src/forum/Forum.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Forum extends React.Component {
+	render() {
+		return (
+			<p>Welcome to the Forum.</p>
+		);
+	}
+}
+
+export default Forum;
