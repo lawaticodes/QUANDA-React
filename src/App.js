b2fd8fb5f97c6acdbc0962d2ed664b844d1a7bda
tinalimbu <tinalawati@gmail.com>
2019-12-07 17:53:18 +0000

    Minor edits to App.js and css

--- src/App.js
+++ src/App.js
@@ -10,17 +10,14 @@ class Quanda extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="Header">
-            <p>This is the header.</p>
+        <div className="menu-icon">
+          <p>MENU</p>
         </div>
-        <div className="Content">
-          <p>Welcome to Quanda.</p>
+        <div className="content">
+          <p>QUANDA</p>
           <Forum/>
           <Bundle/>
           <Release/>
-        </div>
-        <div className="Footer">
-            <p>This is the footer.</p>
         </div>
       </React.Fragment>
     );
