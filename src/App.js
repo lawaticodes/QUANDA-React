65102288e8d48a2c4af9331209450917afd2c5e5
tinalimbu <tinalawati@gmail.com>
2019-12-28 21:40:44 +0000

    Interactive menu

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import React from 'react';
 import { Icon } from 'antd';
 
 import Landing from './landing/Landing.js';
+import LandingMenu from './menus/Menus.js';
 
 import './App.css';
 
 class Quanda extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+  }
+
+  handleMenuClick() {
+    if (this.state.menuOpen) {
+      this.setState({menuOpen: false});
+    } else {
+      this.setState({menuOpen: true});
+    }
+  }
+
   render() {
+    const menuOpen = this.state.menuOpen;
+
     return (
       <React.Fragment>
         <div className="menu">
-          <Icon type="menu"/>
+          <Icon type="menu" onClick={this.handleMenuClick}/>
         </div>
         <div className="content">
-          <Landing/>
+          {menuOpen ? <LandingMenu/> : <Landing/>}
         </div>
       </React.Fragment>
     );
---
+++ src/menus/Menus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LandingMenu extends React.Component {
+  render() {
+    return(
+      <p>This is the landing menu</p>
+    );
+  }
+}
+
+export default LandingMenu;
