069f4ca8492a9233e6e91ad7f9ab68cd04bf1808
tinalimbu <tinalawati@gmail.com>
2019-12-28 18:05:04 +0000

    Edited styling for landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-
-import './App.css';
+import { Button, Icon } from 'antd';
 
 import Bundle from './bundle/Bundle.js';
 import Forum from './forum/Forum.js';
 import Release from './release/Release.js';
 
+import './App.css';
+
 class Quanda extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="menu-icon">
-          <p>MENU</p>
+        <div className="menu">
+        <h2>MENU</h2>
+          <Icon type="menu" className="menu-icon"/>
         </div>
         <div className="content">
-          <div className="title">
-            <h1>QUANDA.</h1>
+          <div className="landing-text">
+            <h1>QUANDA,</h1>
+            <p>a space for open discussions and communication.</p>
+            <div className="landing-buttons">
+              <Button className="button">LOG IN</Button>
+              <p>or</p>
+              <Button className="button">SIGN UP</Button>
+            </div>
           </div>
         </div>
       </React.Fragment>
