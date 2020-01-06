6b2cf31eae42cbddefafbe4cf0253837a16445d1
tinalimbu <tinalawati@gmail.com>
2020-01-06 19:11:27 +0000

    Improved homepage UI

--- src/other/Homepage.js
+++ src/other/Homepage.js
@@ -1,9 +1,32 @@
+import { Icon } from 'antd';
 import React from 'react';
+
+import './Homepage.css';
 
 class Homepage extends React.Component {
   render() {
     return(
-      <p>This is what the homepage looks like when you have logged in.</p>
+      <React.Fragment>
+        <div className="menu-icon-wrapper link">
+          <Icon type="menu" className="menu-icon" onClick={this.handleMenuClick}/>
+        </div>
+        <div className="content">
+          <div className="profile">
+            <img className="avatar" src="insert_image_here.jpg"/>
+            <p>TINA LIMBU</p>
+          </div>
+          <div className="notifications">
+            <div className="bundle-notifications">
+              <p>BUNDLE:</p>
+              <p>0 unread notifications</p>
+            </div>
+            <div className="forum-notifications">
+              <p>FORUM:</p>
+              <p>0 unread notifications</p>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
