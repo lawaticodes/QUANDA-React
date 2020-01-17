6f80936d8b866c3a6c8277faad56b5dc8fb6a6a0
tinalimbu <tinalawati@gmail.com>
2020-01-17 19:05:23 +0000

    POST request now sends through params from sign up form

--- src/other/Forms.js
+++ src/other/Forms.js
@@ -40,8 +40,18 @@ class SignUpForm extends React.Component {
   }
 
   submit() {
+    let userInput = {
+      username: document.getElementById("username").value,
+      password1: document.getElementById("password1").value,
+      password2: document.getElementById("password2").value,
+      email: document.getElementById("email").value
+    };
+    axios.post(
+      "http://127.0.0.1:8000/signup/create_user/", null, {params: userInput}
+    ).then(function(response) {
+      console.log(response);
+    });
     this.props.handleSignedUp();
-    axios.get("http://127.0.0.1:8000/signup/");
   }
 
   render() {
@@ -50,16 +60,16 @@ class SignUpForm extends React.Component {
         <p className="title">SIGN UP</p>
         <div>
           <div>
-            <label>Username:<Input className="input" placeholder="Please enter your username."/></label>
+            <label>Username:<Input className="input" id="username" placeholder="Please enter your username."/></label>
           </div>
           <div>
-            <label>Password:<Input className="input" placeholder="Please enter your password."/></label>
+            <label>Password:<Input className="input" id="password1" placeholder="Please enter your password."/></label>
           </div>
           <div>
-            <label>Re-enter password:<Input className="input" placeholder="Please re-enter your password."/></label>
+            <label>Re-enter password:<Input className="input" id="password2" placeholder="Please re-enter your password."/></label>
           </div>
           <div>
-            <label>Email:<Input className="input" placeholder="Please enter your email."/></label>
+            <label>Email:<Input className="input" id="email" placeholder="Please enter your email."/></label>
           </div>
         </div>
         <Button className="button" onClick={this.submit}>SUBMIT</Button>
