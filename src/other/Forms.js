62222e81fd10321cf28e881fdedbfaeaf9da9d9e
tinalimbu <tinalawati@gmail.com>
2020-03-08 14:21:44 +0000

    Link back to home after sign up success

--- src/other/Forms.js
+++ src/other/Forms.js
@@ -42,6 +42,7 @@ class SignUpForm extends React.Component {
     }
 
     this.submit = this.submit.bind(this);
+    this.goToHomePage = this.goToHomePage.bind(this);
   }
 
   submit = async () => {
@@ -62,6 +63,10 @@ class SignUpForm extends React.Component {
     } catch (error) {
       this.setState({submitted: true, error: error.response.data.message});
     }
+  }
+
+  goToHomePage() {
+    window.location.replace("http://127.0.0.1:3000/");
   }
 
   render() {
@@ -89,13 +94,18 @@ class SignUpForm extends React.Component {
     if (this.state.submitted) {
       if (this.state.error) {
         return(
-          <div>
+          <div className="sign-up-message">
             {signUpForm}
-            <p className="sign-up-message">{this.state.error}</p>
+            <p>{this.state.error}</p>
           </div>
         );
       } else {
-        return(<div>Sign up successful. Please check your email and click on the confirmation link to complete your registration.</div>);
+        return(
+          <div className="flex sign-up-message">
+            <p className="sign-up-success"> Sign up successful. Please check your email and click on the confirmation link to complete your registration.</p>
+            <Button className="button" onClick={this.goToHomePage}>HOME</Button>
+          </div>
+        );
       }
     } else {
       return(<div>{signUpForm}</div>);
